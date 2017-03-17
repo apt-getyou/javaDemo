cffc425b2d7d7f46be1ed55776463e73cbc9a880
liubowen <banhujiu@gmail.com>
2017-03-17 18:26:54 +0800

    Stack

---
+++ src/main/java/NutritionFacts.java
@@ -0,0 +1,80 @@
+/**
+ * 多个构造器参数时可考虑使用构建器
+ * @author 刘博文
+ * @date 2017/2/28 0028 18:36
+ */
+public class NutritionFacts {
+	private final int servingSize;
+
+	private final int servings;
+
+	private final int calories;
+
+	private final int fat;
+
+
+	//构建器
+	public static final class Builder {
+		private int servingSize;
+		private int servings;
+		private int calories;
+		private int fat;
+
+		private Builder() {
+		}
+
+		public static Builder aNutritionFacts() {
+			return new Builder();
+		}
+
+		public Builder withServingSize(int servingSize) {
+			this.servingSize = servingSize;
+			return this;
+		}
+
+		public Builder withServings(int servings) {
+			this.servings = servings;
+			return this;
+		}
+
+		public Builder withCalories(int calories) {
+			this.calories = calories;
+			return this;
+		}
+
+		public Builder withFat(int fat) {
+			this.fat = fat;
+			return this;
+		}
+
+		public Builder but() {
+			return aNutritionFacts().withServingSize(servingSize).withServings(servings).withCalories(calories).withFat(fat);
+		}
+
+		public NutritionFacts build() {
+			return new NutritionFacts(this);
+		}
+	}
+
+	private NutritionFacts(Builder builder){
+		servings = builder.servings;
+		calories = builder.calories;
+		servingSize = builder.servingSize;
+		fat = builder.fat;
+	}
+
+	@Override
+	public String toString() {
+		return "NutritionFacts{" +
+				"servingSize=" + servingSize +
+				", servings=" + servings +
+				", calories=" + calories +
+				", fat=" + fat +
+				'}';
+	}
+
+	public static void main(String[] args) {
+		NutritionFacts nutrition = new NutritionFacts.Builder().withCalories(12).withCalories(13).build();
+		System.out.print(nutrition);
+	}
+}
---
+++ src/main/java/Stack.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * 清空过期的对象引用
+ *
+ * @author 刘博文
+ * @date 2017/2/28 0028 22:05
+ */
+public class Stack {
+	private Object[] elements;
+
+	private int size = 0;
+
+	private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+	public Stack() {
+		elements = new Object[DEFAULT_INITIAL_CAPACITY];
+	}
+
+	public void push(Object e) {
+		ensureCapacity();
+		elements[size++] = e;
+	}
+
+	/**
+	 * 此push方法在使用上不会显现出任何问题，但存在对对象的过期引用问题，导致内存泄露，且这种内存泄露十分的隐蔽
+	 */
+	public Object pop() {
+		if (size == 0) {
+			throw new EmptyStackException();
+		}
+		return elements[--size];//此处存在内存泄露，栈收缩时没有清空被弹出的对象，导致对象的过期引用
+	}
+
+	/**
+	 * 不存在内存泄露的方法
+	 */
+	public Object popTrue() {
+		if (size == 0) {
+			throw new EmptyStackException();
+		}
+		Object result = elements[--size];
+		elements[size] = null; //清空过期引用
+		return result;
+	}
+
+	//在栈不够时扩大栈的容量
+	private void ensureCapacity() {
+		if (elements.length == size) {
+			elements = Arrays.copyOf(elements, 2 * size + 1);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Stack{" +
+				"elements=" + Arrays.toString(elements) +
+				", size=" + size +
+				'}';
+	}
+
+	@Override
+	protected Stack clone() throws CloneNotSupportedException {
+		Stack result = (Stack) super.clone();
+		result.elements = elements.clone();
+		return result;
+	}
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Stack stack1 = new Stack();
+		System.out.print(stack1.clone() != stack1);
+	}
+}
