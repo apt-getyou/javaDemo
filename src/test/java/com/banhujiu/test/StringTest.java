ca6eb2083f2d20f30f15d5f326d763e6dd25455a
liubowen <banhujiu@gmail.com>
2017-09-18 11:05:03 +0800

    String

--- src/test/java/com/banhujiu/test/StringTest.java
+++ src/test/java/com/banhujiu/test/StringTest.java
@@ -93,28 +93,70 @@ public class StringTest {
 		int begin = 0;
 		int size = 50000;
 		{//String
-			String s = "";
 			long beginTime = System.currentTimeMillis();
 			for (int i = begin; i < size; i++) {
-				s += String.valueOf(i);
+				String s = String.valueOf(i);
 			}
-			logger.info("第一次String操作字符对象,用时{}", System.currentTimeMillis() - beginTime);
+			logger.info("第一次String生成对象,用时{}", System.currentTimeMillis() - beginTime);
 		}
 
 		{//String
-			String s = "";
 			long beginTime = System.currentTimeMillis();
 			for (int i = begin; i < size; i++) {
-				s += String.valueOf(i);
+				String s = String.valueOf(i);
 			}
-			logger.info("第二次String操作字符对象,用时{}", System.currentTimeMillis() - beginTime);
+			logger.info("第二次String生成对象,用时{}", System.currentTimeMillis() - beginTime);
+		}
+
+		{//String
+			long beginTime = System.currentTimeMillis();
+			for (int i = begin; i < size; i++) {
+				String s = String.valueOf(i);
+			}
+			logger.info("第三次String生成对象,用时{}", System.currentTimeMillis() - beginTime);
 		}
 		/*
-		16:12:56.028 [main] INFO  com.banhujiu.test.StringTest - 第一次String操作字符对象,用时6881
-		16:13:02.164 [main] INFO  com.banhujiu.test.StringTest - 第二次String操作字符对象,用时6133
-
-		相同的代码，第二次的运行速度明显快了不少
-		//TODO 解释原因
+		10:39:24.833 [main] INFO  com.banhujiu.test.StringTest - 第一次String操作字符对象,用时7
+		10:39:24.841 [main] INFO  com.banhujiu.test.StringTest - 第二次String操作字符对象,用时3
+		10:39:24.844 [main] INFO  com.banhujiu.test.StringTest - 第二次String操作字符对象,用时3
+		相同的代码，第二次的运行速度明显快了不少，而后运行速度相近了（不一定相同）
+		原因：由于String对象的不可变性，java在新建String对象时，会先在jvm内存内寻找是否存在相同的值，
+		如果存在，则直接引用，而不去分配新的内存并生成新的对象，所以第二次和第三次的速度才会相近且比第一次快
 		 */
 	}
+
+
+	@Test
+	@SuppressWarnings({"StringEquality", "StringBufferReplaceableByString", "ConstantConditions", "RedundantStringConstructorCall"})
+	public void test_string_create() {
+		String a = new StringBuilder("中国").append("很大").toString();
+		System.out.println(a.intern() == a);//true
+		String b = new StringBuilder("ja").append("va").toString();
+		System.out.println(b.intern() == b);//false
+
+
+		String c = "中国" + "很大";
+		System.out.println(c.intern() == c);//true
+		String d = "ja" + "va";
+		System.out.println(d.intern() == d);//true
+
+
+		String e = "中国";
+		String f = "中国";
+		System.out.println(e == f);//true
+
+		String i = new String("中国");
+		String j = new String("中国");
+		System.out.println(i == j);//false
+		/*
+		使用 new String() 方法会生成新的String对象，即使内存内已经有了相同的String对象，
+		所以导致 i!=j,由于String对象的不可变性，在平时编码时，不推荐使用new String方式产生String对象，
+		直接使用 String i = "" 方式，可以节省内存空间，提高运行效率，但也正因为String的不可变性，
+		一旦一个String对象在内存中创建，它将是不可改变的，所有的String类中方法并不是改变String对象自己，
+		而是重新创建一个新的String对象，所以在频繁操作String对象时，推荐使用
+		StringBuild，如果存在线程问题，则使用StringBuffer。
+		 */
+	}
+
+
 }
