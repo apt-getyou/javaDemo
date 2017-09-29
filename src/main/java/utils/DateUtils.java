a4b1c9043e3ec30f4b37359c6f122de8edd2fb37
liubowen <banhujiu@gmail.com>
2017-09-29 18:36:53 +0800

    时间工具

--- src/main/java/utils/DateUtils.java
+++ src/main/java/utils/DateUtils.java
@@ -25,14 +25,51 @@ public class DateUtils {
 
 		min.setTime(beginMonth);
 		max.setTime(endMonth);
-		
+
 		while (min.compareTo(max) <= 0) {
 			Integer format = Integer.valueOf(sdf.format(min.getTime()));
-			System.out.println(format);
 			result.add(format);
 			min.add(Calendar.MONTH, 1);
 		}
 		return result;
 	}
 
+	public static String integerToYearMonth(Integer date) {
+		if (date == null) {
+			return "";
+		}
+		String yearMonth = String.valueOf(date);
+		if (yearMonth.length() != 6) {
+			throw new RuntimeException("数据格式错误");
+		}
+		String year = yearMonth.substring(0, 4);
+		String month = yearMonth.substring(4, 6);
+		if (month.startsWith("0")) {
+			month = month.substring(1, 2);
+		}
+		return year + "年" + month + "月";
+	}
+
+
+	public static String getTheBeginDayFotMoth(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR, -12);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(calendar.getTime());
+	}
+
+	public static String getTheEndDayFotMoth(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR, 11);
+		calendar.set(Calendar.MINUTE, 59);
+		calendar.set(Calendar.SECOND, 59);
+		calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(calendar.getTime());
+	}
 }
--- src/test/java/com/banhujiu/test/TimeUnitTest.java
+++ src/test/java/com/banhujiu/test/TimeUnitTest.java
@@ -3,8 +3,14 @@ package com.banhujiu.test;
 import utils.DateUtils;
 import utils.enums.DateFormatEnum;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
@@ -26,6 +32,11 @@ public class TimeUnitTest {
 
 	@Test
 	public void test_getYearMonth() {
+		List<Integer> list = getLastYearToNow();
+		logger.info(list);
+	}
+
+	private List<Integer> getLastYearToNow() {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(new Date());
 		Date endMonth = calendar.getTime();
@@ -33,13 +44,58 @@ public class TimeUnitTest {
 		calendar.add(Calendar.YEAR, -1);
 		Date beginMonth = calendar.getTime();
 
-		logger.info(DateUtils.getYearMonthList(beginMonth, endMonth));
+		return DateUtils.getYearMonthList(beginMonth, endMonth);
 	}
 
 
 	@Test
 	public void test_dataFormatEnum() {
 		logger.info("时间为" + DateFormatEnum.formatDate("456555"));
+	}
+
+	@Test
+	public void test_integerToYearMonth(){
+		logger.info(DateUtils.integerToYearMonth(201806));
+		logger.info(DateUtils.integerToYearMonth(201807));
+		logger.info(DateUtils.integerToYearMonth(201812));
+	}
+
+	@Test
+	public void test_draw_table_for_month_and_customer() {
+		List<Integer> monthList = getLastYearToNow();
+		List<Long> idList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			idList.add((long) i);
+		}
+		TreeMap<Long, TreeMap<Integer, BigDecimal>> map = new TreeMap<>();
+		for (Long id : idList) {
+			map.put(id, new TreeMap<>());
+		}
+		Random random = new Random();
+		for (Map.Entry<Long, TreeMap<Integer, BigDecimal>> entry : map.entrySet()) {
+			for (Integer month : monthList) {
+				entry.getValue().put(month, new BigDecimal(random.nextInt(9)));
+			}
+		}
+		StringBuilder sb = new StringBuilder("| 客户\\月份 |");
+		for (Integer month : monthList) {
+			sb.append(" ").append(month).append(" |");
+		}
+		System.out.println(sb.toString());
+		for (Map.Entry<Long, TreeMap<Integer, BigDecimal>> entry : map.entrySet()) {
+			StringBuilder tempSb = new StringBuilder("| 客户id：").append(entry.getKey().toString()).append(" |");
+			for (Map.Entry<Integer, BigDecimal> decimalEntry : entry.getValue().entrySet()) {
+				tempSb.append("   ").append(decimalEntry.getValue()).append("    |");
+			}
+			System.out.println(tempSb.toString());
+		}
+	}
+
+
+	@Test
+	public void test(){
+		logger.info(DateUtils.getTheEndDayFotMoth(new Date()));
+		logger.info(DateUtils.getTheBeginDayFotMoth(new Date()));
 	}
 
 }
